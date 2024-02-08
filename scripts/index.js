f2c3d41153c003277b6297e9fb7727ad7e3b34e4
ecomeel <ecomeel@gmail.com>
2024-02-08 16:45:40 +0300

    dropDown footer

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,24 @@
-new Swiper('.slider', {
+new Swiper(".slider", {
   scrollbar: {
-    el: '.swiper-scrollbar',
+    el: ".swiper-scrollbar",
   },
-})
+});
+
+// Footer dropDown
+const footerShortBodyNode = document.getElementById("footerShortBody");
+const footerFullBodyNode = document.getElementById("footerFullBody");
+const footerChangeVisibleBtnNode = document.getElementById(
+  "footerChangeVisibleBtn"
+);
+function handleChangeVisibleFooter() {
+  if (this.innerText === 'Read more') {
+    this.innerText = 'Read less';
+    footerFullBodyNode.classList.add('show-block');
+    footerShortBodyNode.classList.add('hide-block')
+  } else {
+    this.innerText = 'Read more';
+    footerFullBodyNode.classList.remove('show-block');
+    footerShortBodyNode.classList.remove('hide-block')
+  }
+}
+footerChangeVisibleBtnNode.addEventListener('click', handleChangeVisibleFooter)
